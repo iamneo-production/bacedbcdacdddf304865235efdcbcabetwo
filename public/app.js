ba294e4c406c5067f6d4f52eb095d112e428fc4e
Iamneo <techadmin@iamneo.ai>
2023-09-09 14:31:36 +0000

    Backup Commit

--- public/app.js
+++ public/app.js
@@ -1,37 +1,61 @@
+let currentPlayer = 'X';
+let gameBoard = ['', '', '', '', '', '', '', '', ''];
+let gameActive = true;
 
-        // JavaScript code for Tic Tac Toe game logic
-        let currentPlayer = 'X';
-        let isGameActive = true;
+const boxes = document.querySelectorAll('.btn');
+const resultText = document.querySelector('.result');
 
-        // Function to handle player moves
-        function handleMove(element) {
-            if (element.value === '' && isGameActive) {
-                element.value = currentPlayer;
-                element.disabled = true;
-                togglePlayer();
-                checkWin();
-            }
-        }
-
-        // Function to toggle between X and O
-        function togglePlayer() {
+function handleMove(index) {
+    if (gameBoard[index] === '' && gameActive) {
+        gameBoard[index] = currentPlayer;
+        boxes[index].value = currentPlayer;
+        boxes[index].disabled = true;
+        
+        if (checkWin() === currentPlayer) {
+            resultText.textContent = `Player ${currentPlayer} wins!`;
+            gameActive = false;
+        } else if (!gameBoard.includes('')) {
+            resultText.textContent = "It's a draw!";
+            gameActive = false;
+        } else {
             currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
-            document.querySelector('.result').textContent = `Player ${currentPlayer}'s turn`;
+            resultText.textContent = `Player ${currentPlayer}'s Turn`;
         }
+    }
+}
 
-        // Function to check for a winning condition
-        function checkWin() {
-            // Implement your winning condition logic here
-        }
+function checkWin() {
+    const winPatterns = [
+        [0, 1, 2], [3, 4, 5], [6, 7, 8], // Rows
+        [0, 3, 6], [1, 4, 7], [2, 5, 8], // Columns
+        [0, 4, 8], [2, 4, 6] // Diagonals
+    ];
 
-        // Function to reset the game
-        function resetGame() {
-            const buttons = document.querySelectorAll('.bt');
-            buttons.forEach(button => {
-                button.value = '';
-                button.disabled = false;
-            });
-            currentPlayer = 'X';
-            isGameActive = true;
-            document.querySelector('.result').textContent = `Player ${currentPlayer}'s turn`;
+    for (const pattern of winPatterns) {
+        const [a, b, c] = pattern;
+        if (gameBoard[a] && gameBoard[a] === gameBoard[b] && gameBoard[a] === gameBoard[c]) {
+            return gameBoard[a];
         }
+    }
+
+    return null;
+}
+
+function resetGame() {
+    gameBoard = ['', '', '', '', '', '', '', '', ''];
+    gameActive = true;
+    currentPlayer = 'X';
+    boxes.forEach(box => {
+        box.value = '';
+        box.disabled = false;
+    });
+    resultText.textContent = `Player ${currentPlayer}'s Turn`;
+}
+
+boxes.forEach((box, index) => {
+    box.addEventListener('click', () => {
+        handleMove(index);
+    });
+});
+
+resultText.textContent = `Player ${currentPlayer}'s Turn`;
