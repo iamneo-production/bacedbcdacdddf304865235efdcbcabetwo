483038b255461546228f46b0bd83dd8059d5f1ea
Iamneo <techadmin@iamneo.ai>
2023-09-09 14:04:59 +0000

    Backup Commit

--- public/app.js
+++ public/app.js
@@ -1,56 +1,58 @@
-// JavaScript code for the Tic Tac Toe game
-const cells = document.querySelectorAll('.bt');
-const resultText = document.querySelector('.result-text');
-const resetButton = document.getElementById('reset-button');
-let currentPlayer = 'X';
-let gameBoard = ['', '', '', '', '', '', '', '', ''];
+et currentPlayer = 'X';
+const cells = document.querySelectorAll('.btn');
+const resultDisplay = document.getElementById('result');
 let gameActive = true;
 
-const winningCombinations = [
-    [0, 1, 2], [3, 4, 5], [6, 7, 8],
-    [0, 3, 6], [1, 4, 7], [2, 5, 8],
-    [0, 4, 8], [2, 4, 6]
+// Winning combinations
+const winPatterns = [
+    [0, 1, 2], [3, 4, 5], [6, 7, 8], // Rows
+    [0, 3, 6], [1, 4, 7], [2, 5, 8], // Columns
+    [0, 4, 8], [2, 4, 6] // Diagonals
 ];
 
-const checkWinner = () => {
-    for (let combo of winningCombinations) {
-        const [a, b, c] = combo;
-        if (gameBoard[a] && gameBoard[a] === gameBoard[b] && gameBoard[a] === gameBoard[c]) {
-            resultText.textContent = `Player ${gameBoard[a]} wins!`;
+// Function to check for a win
+function checkWin() {
+    for (const pattern of winPatterns) {
+        const [a, b, c] = pattern;
+        if (cells[a].textContent && cells[a].textContent === cells[b].textContent && cells[a].textContent === cells[c].textContent) {
+            return true;
+        }
+    }
+    return false;
+}
+
+// Function to handle cell click
+function handleCellClick(event) {
+    const cell = event.target;
+
+    // Check if the cell is empty and the game is still ongoing
+    if (cell.textContent === '' && gameActive) {
+        cell.textContent = currentPlayer;
+
+        // Check for a win
+        if (checkWin()) {
+            resultDisplay.textContent = `Player ${currentPlayer} wins!`;
             gameActive = false;
-            resetButton.disabled = false;
-        }
-    }
-
-    if (!gameBoard.includes('') && gameActive) {
-        resultText.textContent = "It's a draw!";
-        resetButton.disabled = false;
-    }
-};
-
-const handleClick = (cell, index) => {
-    if (gameBoard[index] === '' && gameActive) {
-        cell.value = currentPlayer;
-        gameBoard[index] = currentPlayer;
-        checkWinner();
-        if (gameActive) {
+        } else if ([...cells].every(cell => cell.textContent)) {
+            resultDisplay.textContent = "It's a draw!";
+            gameActive = false;
+        } else {
             currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
-            resultText.textContent = `Player ${currentPlayer}'s turn`;
+            resultDisplay.textContent = `Player ${currentPlayer}'s turn`;
         }
     }
-};
+}
 
-cells.forEach((cell, index) => {
-    cell.addEventListener('click', () => handleClick(cell, index));
-});
-
-resetButton.addEventListener('click', () => {
-    cells.forEach((cell, index) => {
-        cell.value = '';
-        gameBoard[index] = '';
-    });
+// Function to reset the game
+function resetGame() {
+    cells.forEach(cell => cell.textContent = '');
     currentPlayer = 'X';
-    resultText.textContent = `Player ${currentPlayer}'s turn`;
-    resetButton.disabled = true;
+    resultDisplay.textContent = "Player X's turn";
     gameActive = true;
-})
+}
+
+// Add click event listeners to cells
+cells.forEach(cell => cell.addEventListener('click', handleCellClick));
+
+// Add click event listener to reset button
+document.getElementById('reset-button').addEventListener('click', resetGame);
