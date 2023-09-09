c4f389bed15cfc9c555035d0e07e1a50a15a8ba0
Iamneo <techadmin@iamneo.ai>
2023-09-09 14:26:35 +0000

    Backup Commit

--- public/app.js
+++ public/app.js
@@ -1,75 +1,37 @@
-// Initial game state
-let cells = ['', '', '', '', '', '', '', '', ''];
-let currentPlayer = 'X';
-let result = document.querySelector('.result');
-let btns = document.querySelectorAll('.btn');
-let conditions = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-];
 
-// Function to handle player moves
-const ticTacToe = (element, index) => {
-    // Check if the cell is empty and the game is still active
-    if (cells[index] === '' && !result.textContent.includes('wins')) {
-        // Update the cell with the current player's symbol
-        cells[index] = currentPlayer;
-        element.textContent = currentPlayer;
+        // JavaScript code for Tic Tac Toe game logic
+        let currentPlayer = 'X';
+        let isGameActive = true;
 
-        // Check for winning conditions
-        for (const condition of conditions) {
-            const [a, b, c] = condition;
-            if (cells[a] && cells[a] === cells[b] && cells[a] === cells[c]) {
-                result.textContent = `Player ${currentPlayer} wins!`;
-                disableButtons();
-                return;
+        // Function to handle player moves
+        function handleMove(element) {
+            if (element.value === '' && isGameActive) {
+                element.value = currentPlayer;
+                element.disabled = true;
+                togglePlayer();
+                checkWin();
             }
         }
 
-        // Check for a draw
-        if (!cells.includes('')) {
-            result.textContent = "It's a draw!";
-            disableButtons();
-            return;
+        // Function to toggle between X and O
+        function togglePlayer() {
+            currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+            document.querySelector('.result').textContent = `Player ${currentPlayer}'s turn`;
         }
 
-        // Switch to the next player
-        currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
-        result.textContent = `Player ${currentPlayer}'s turn`;
-    }
-};
+        // Function to check for a winning condition
+        function checkWin() {
+            // Implement your winning condition logic here
+        }
 
-// Function to disable buttons after a win or draw
-const disableButtons = () => {
-    btns.forEach((btn) => {
-        btn.disabled = true;
-    });
-};
-
-// Function to reset the game
-const resetGame = () => {
-    // Reset game state
-    cells = ['', '', '', '', '', '', '', '', ''];
-    currentPlayer = 'X';
-
-    // Clear the result text
-    result.textContent = `Player ${currentPlayer}'s turn`;
-
-    // Enable buttons
-    btns.forEach((btn) => {
-        btn.textContent = '';
-        btn.disabled = false;
-    });
-};
-
-btns.forEach((btn, i) => {
-    btn.addEventListener('click', () => ticTacToe(btn, i));
-});
-
-document.querySelector('#reset').addEventListener('click', resetGame);
+        // Function to reset the game
+        function resetGame() {
+            const buttons = document.querySelectorAll('.bt');
+            buttons.forEach(button => {
+                button.value = '';
+                button.disabled = false;
+            });
+            currentPlayer = 'X';
+            isGameActive = true;
+            document.querySelector('.result').textContent = `Player ${currentPlayer}'s turn`;
+        }
