e3867c40d7f0fd5022a0a23e04467010cabe6d10
Iamneo <techadmin@iamneo.ai>
2023-09-09 14:09:59 +0000

    Backup Commit

--- public/app.js
+++ public/app.js
@@ -1,17 +1,16 @@
-et currentPlayer = 'X';
+let currentPlayer = 'X';
 const cells = document.querySelectorAll('.btn');
 const resultDisplay = document.getElementById('result');
 let gameActive = true;
 
-// Winning combinations
-const winPatterns = [
-    [0, 1, 2], [3, 4, 5], [6, 7, 8], // Rows
-    [0, 3, 6], [1, 4, 7], [2, 5, 8], // Columns
-    [0, 4, 8], [2, 4, 6] // Diagonals
-];
-
 // Function to check for a win
 function checkWin() {
+    const winPatterns = [
+        [0, 1, 2], [3, 4, 5], [6, 7, 8], // Rows
+        [0, 3, 6], [1, 4, 7], [2, 5, 8], // Columns
+        [0, 4, 8], [2, 4, 6] // Diagonals
+    ];
+
     for (const pattern of winPatterns) {
         const [a, b, c] = pattern;
         if (cells[a].textContent && cells[a].textContent === cells[b].textContent && cells[a].textContent === cells[c].textContent) {
